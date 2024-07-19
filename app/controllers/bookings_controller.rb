3144ce90360887220879f921d7e363e3e0fbd1a3
Lucas Siviglia <siviglialucas@gmail.com>
2024-07-20 08:34:16 +1000

    need to log in to make a booking message

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def new
     @booking = Booking.new
     @pokemon = Pokemon.find(params[:pokemon_id])
@@ -44,4 +45,11 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to new_user_session_path, alert: 'You need to log in to make a booking.'
+    end
+  end
+
 end
